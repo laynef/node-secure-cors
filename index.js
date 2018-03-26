621dcd20828a4fd2be1c27b1ee99948268eb2824
Layne Faler <laynefaler@Laynes-MacBook-Pro.local>
2018-03-25 20:26:56 -0700

    Updates to package json

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const { error } = require('util');
     var vary = require('vary');
 
     var defaults = {
-        origin: '*',
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         preflightContinue: false,
         optionsSuccessStatus: 204
